e88419b3e66056abc5081c72710d4ae058621206
Martino Visintin <mvisinti@thoughtworks.com>
2015-02-12 15:31:12 -0500

    vanity refactor

--- src/publishing.js
+++ src/publishing.js
@@ -1,7 +1,7 @@
 var utils = require('./utils');
 var sh = require('shelljs');
 
-var __git_publishing_remote_name = 'publishing';
+var publishing_remote = 'publishing';
 
 module.exports = {
 
@@ -10,9 +10,7 @@ module.exports = {
 
       sh.exec('git init ' + path);
 
-      var cmd = 'git remote add ' + __git_publishing_remote_name + ' ' +
-        publishing_url;
-
+      var cmd = 'git remote add ' + publishing_remote + ' ' + publishing_url;
       var git_remote_add = sh.exec(cmd);
       if (git_remote_add.code === 128) {
         sh.echo(('--> Remote publishing already set-up. Skipping.').yellow);
@@ -29,23 +27,25 @@ module.exports = {
   push: function(compiled_blog_path) {
     utils.with_cwd(compiled_blog_path, function(cwd) {
 
-      var command = 'git add ' + cwd;
-      sh.echo("-> " + command);
-      sh.exec(command);
+      var cmd = 'git add ' + cwd;
+      sh.echo("-> " + cmd);
+      sh.exec(cmd);
 
-      command = 'git commit -m "update blog"';
-      sh.echo("-> " + command);
-      sh.exec(command);
+      cmd = 'git commit -m "update blog"';
+      sh.echo("-> " + cmd);
+      sh.exec(cmd);
 
-      command = 'git push ' + __git_publishing_remote_name + ' master';
-      sh.echo("-> " + command);
-      var git_push = sh.exec(command);
+      cmd = 'git push ' + publishing_remote + ' master';
+      sh.echo("-> " + cmd);
+      var git_push = sh.exec(cmd);
 
       if (git_push.code !== 0) {
-        sh.echo(('==> Could not push to remote `' +
-                 __git_publishing_remote_name + '`. Aborting.').red);
-        sh.echo('Command used: ' + command);
-        sh.echo('Command output: ' + command.output);
+        var msg = '==> Could not push to remote `' +
+                  publishing_remote +
+                  '`. Aborting.';
+        sh.echo(msg.red);
+        sh.echo('Command used: ' + cmd);
+        sh.echo('Command output: ' + cmd.output);
         sh.exit(1);
       }
 
