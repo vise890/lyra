161d83c611a54e8f0d86b71ad301b23fae38c4dd
Martino Visintin <mvisinti@thoughtworks.com>
2015-02-13 13:25:39 -0500

    allow for prerequisites export to be called directly

--- src/prerequisites.js
+++ src/prerequisites.js
@@ -1,11 +1,13 @@
 var sh = require('shelljs');
 var colors = require('colors');
 
-module.exports = {
-  check: function() {
-    if (!sh.which('git')) {
-      sh.echo('==> Sorry, Lyra needs git to run (for now)'.red);
-      sh.exit(1);
-    }
+var check = function() {
+  if (!sh.which('git')) {
+    sh.echo('==> Sorry, Lyra needs git to run (for now)'.red);
+    sh.exit(1);
   }
 };
+
+check.check = check;
+
+module.exports = check;
